9182b27de0b2b6c7e7c422278c77024150809108
Soe Myat Cho <soemyatchobgn@gmail.com>
2025-05-04 16:11:39 -0700

    Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Project from "./components/Project";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Project />
     </>
   );
 }
